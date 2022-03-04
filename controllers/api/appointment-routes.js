5ce8fba92b9b680b7b05b5b1b577ae4ee0cb54fe
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-03-04 16:27:53 -0500

    front and back end api routes for appointment scheduler

--- controllers/api/appointment-routes.js
+++ controllers/api/appointment-routes.js
@@ -1,27 +1,57 @@
 const router = require('express').Router();
-const { User, Schedule, Repairs } = require('../../models');
+const { User, Schedule, Car, Repairs } = require('../../models');
 const withAuth = require('../../utils/auth');
 // need a post request that allows user to post their apoointment data to server
 // sign up page
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
+    Schedule.create({
+      Date: req.body.date,
+      user_id: req.session.id,
+    //   I need to pass through car id here as well but not exactly sure how to accomplish that car_id: 
     })
-      .then(dbUserData => {
-        req.session.save(() => {
-          req.session.user_id = dbUserData.id;
-          req.session.username = dbUserData.username;
-          req.session.loggedIn = true;
-    
-          res.json(dbUserData);
-        });
+      .then(apptData => {
+        res.json(apptData);
       })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
   });
+// A second post request will be needed to add the car to the database
+// is it easier to make two separate post requests or combine it all into one?
+router.post('/', (req, res) => {
+    Car.create({
+      make: req.body.model,
+      vin: req.body.vin,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      year: req.body.mileage,
+      user_id: req.session.id
+    })
+      .then(carData => {
+        res.json(carData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// do I also need to make a third post request to repairs to update what repairs are being done?
+// router.post('/', (req, res) => {
+//     Repairs.create({
+//       item: req.body.model,
+//       status_id: req.body.status_id,
+//       user_id: req.session.id
+//     })
+//       .then(carData => {
+//         res.json(carData);
+//       })
+//       .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+//   });
+
 module.exports = router;
--- public/javascript/schedule-appt.js
+++ public/javascript/schedule-appt.js
@@ -2,19 +2,35 @@ async function appointmentHandler(event) {
     event.preventDefault();
     // this portion is going to need to collect all the data that the user has inputted on the form
     // the query selectors will need to be updated once the template has been completed
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
+
+    // this will be the car data that is passed through
+    const year = document.querySelector('#year').value.trim();
+    const make = document.querySelector('#make').value.trim();
+    const model = document.querySelector('#model').value.trim();
+    const vin = document.querySelector('#vin').value.trim();
+    const mileage = document.querySelector('#mileage').value.trim();
+    // this will be the repair info passed through
+    // not sure how this will be passed through? I assume as an array since user
+    // can pick from a variety of options with checkboxes
+    const repair = []
+    // this will be the appointment info passed through
+    const date = document.querySelector('#date').value.trim();
+    // validate that the user has passed in all necessary fields before the can make an appointment
+    if (year && make && model && vin && repair && date && mileage) {
+      const response = await fetch('/api/appointment-routes', {
         method: 'post',
         body: JSON.stringify({
-          email,
-          password
+          year,
+          make,
+          model,
+          vin,
+          repair,
+          date,
+          mileage
         }),
         headers: { 'Content-Type': 'application/json' }
       });
-      // replace file name here with handlebars that is created
+      // once user has submitted appt, take them back to which page? dashboard?
       if (response.ok) {
         document.location.replace('/dashboard/');
       } else {
@@ -23,4 +39,4 @@ async function appointmentHandler(event) {
     }
   }
 // update these 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', appointmentHandler);
