77fff95446e68409682dcba346315d2739391b01
Mary Braunger <maryjezek@aol.com>
2022-03-06 11:55:56 -0500

    changed signup form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,56 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const withAuth = require("../utils/auth");
 
 // the home page is generic and will be shown to everyone
 // need to either login or signup
-router.get('/', (req, res) => {
-      res.render('home');
-    });
+router.get("/home", (req, res) => {
+  res.render("home");
+});
 
-router.post('/logout', (req, res) => {
+//MB create render login
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//MB create render login
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+//MB create render status
+router.get("/status", (req, res) => {
+  res.render("status");
+});
+
+//MB create render schedule
+router.get("/schedule", (req, res) => {
+  res.render("schedule");
+});
+
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  }
-  else {
+  } else {
     res.status(404).end();
   }
 });
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+
+
+
 
 module.exports = router;
--- public/javascript/login-signup.js
+++ public/javascript/login-signup.js
@@ -1,79 +1,126 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+// function setFormMessage(formElement, type, message) {
+//   const messageElement = formElement.querySelector(".form__message");
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
+//   messageElement.textContent = message;
+//   messageElement.classList.remove(
+//     "form__message--success",
+//     "form__message--error"
+//   );
+//   messageElement.classList.add(`form__message--${type}`);
+// }
 
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
+// function setInputError(inputElement, message) {
+//   inputElement.classList.add("form__input--error");
+//   inputElement.parentElement.querySelector(
+//     ".form__input-error-message"
+//   ).textContent = message;
+// }
 
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
+// function clearInputError(inputElement) {
+//   inputElement.classList.remove("form__input--error");
+//   inputElement.parentElement.querySelector(
+//     ".form__input-error-message"
+//   ).textContent = "";
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const loginForm = document.querySelector("#login");
+//   const createAccountForm = document.querySelector("#createAccount");
 
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
+//   document
+//     .querySelector("#linkCreateAccount")
+//     .addEventListener("click", (e) => {
+//       e.preventDefault();
+//       loginForm.classList.add("form--hidden");
+//       createAccountForm.classList.remove("form--hidden");
+//     });
 
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
+//   document.querySelector("#linkLogin").addEventListener("click", (e) => {
+//     e.preventDefault();
+//     loginForm.classList.remove("form--hidden");
+//     createAccountForm.classList.add("form--hidden");
+//   });
 
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
+//   loginForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
-        // Perform your Fetch login
+//     // Perform your Fetch login
 
-        // async function loginFormHandler(event) {
-        //     event.preventDefault();
+//     // async function loginFormHandler(event) {
+//     //     event.preventDefault();
 
-        // const email = document.querySelector('#email-login').value.trim();
-        // const password = document.querySelector('#password-login').value.trim();
+//     // const email = document.querySelector('#email-login').value.trim();
+//     // const password = document.querySelector('#password-login').value.trim();
 
-        // if (email && password) {
-        //     const response = await fetch('/api/users/login', {
-        //         method: 'post',
-        //         body: JSON.stringify({
-        //             email,
-        //             password
-        //         }),
-        //         headers: { 'Content-Type': 'application/json' }
-        //     });
+//     // if (email && password) {
+//     //     const response = await fetch('/api/users/login', {
+//     //         method: 'post',
+//     //         body: JSON.stringify({
+//     //             email,
+//     //             password
+//     //         }),
+//     //         headers: { 'Content-Type': 'application/json' }
+//     //     });
 
-        //     if (response.ok) {
-        //         document.location.replace('/home');
-        //     } else {
-        //         alert(response.statusText);
-        //     }
-        // }
-        // }
-        // finish fetch login
+//     //     if (response.ok) {
+//     //         document.location.replace('/home');
+//     //     } else {
+//     //         alert(response.statusText);
+//     //     }
+//     // }
+//     // }
+//     // finish fetch login
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
+//     setFormMessage(loginForm, "error", "Invalid password/password combination");
+//   });
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 5 characters in length");
-            }
-        });
+//   document.querySelectorAll(".form__input").forEach((inputElement) => {
+//     inputElement.addEventListener("blur", (e) => {
+//       if (
+//         e.target.id === "signupUsername" &&
+//         e.target.value.length > 0 &&
+//         e.target.value.length < 10
+//       ) {
+//         setInputError(
+//           inputElement,
+//           "Username must be at least 5 characters in length"
+//         );
+//       }
+//     });
 
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
+//     inputElement.addEventListener("input", (e) => {
+//       clearInputError(inputElement);
+//     });
+//   });
+//});
+
+
+//MB added this to handle sign up functionality
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup");
+  const email = document.querySelector("#email-signup");
+  const password = document.querySelector("#password-signup");
+console.log("SIGNING UP")
+  const response = await fetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/status");
+    console.log("ok")
+  } else {
+    console.log("Sign up failed");
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
